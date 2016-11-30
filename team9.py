03e795f446c9f7b30d4bd178afa50f992e58a806
HubMaster9000 <danielsoares@claytonschools.net>
2016-11-30 11:44:31 -0600

    something

--- team9.py
+++ team9.py
@@ -25,8 +25,15 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0:
+        return 'b'
+    else:
+        if 'b' == their_history[-1]:
+            print 'stop it'
+        if 'c' in their_history:
+            return 'b'
+        else:
+            return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
